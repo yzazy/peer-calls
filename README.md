adc377b8855fd7763f5571ad7bab2fb1e6d63e87
Your Name <you@example.com>
2018-07-03 21:25:48 +0700

    add

---
+++ README.md
@@ -0,0 +1,66 @@
+# Peer Calls
+
+[![Build Status](https://travis-ci.org/jeremija/peer-calls.svg?branch=master)](https://travis-ci.org/jeremija/peer-calls)
+
+WebRTC peer to peer calls for everyone. See it live in action at
+[peercalls.com](https://peercalls.com).
+
+Work in progress.
+
+# Requirements
+ - Node.js 8 [https://nodejs.org/en/](https://nodejs.org/en/)
+
+# Installation & Running
+
+From git source:
+
+```bash
+git clone https://github.com/jeremija/peer-calls.git
+cd peer-calls
+npm install
+
+# for production
+npm run build
+npm start
+
+# for development
+npm run start:watch
+```
+
+To run a development version, type:
+
+
+
+If you successfully completed the above steps, your commandline/terminal should
+show that your node server is listening.
+
+On your other machine or mobile device open the url:
+
+```bash
+http://<your_ip_or_localhost>:3000
+
+# Testing
+
+```bash
+npm install
+npm test
+```
+
+# Browser Support
+
+Tested on Firefox and Chrome, including mobile versions.
+
+Does not work on iOS 10, but should work on iOS 11 (untested).
+
+For more details, see here:
+
+- http://caniuse.com/#feat=rtcpeerconnection
+- http://caniuse.com/#search=getUserMedia
+
+# Contributing
+
+See [Contributing](CONTRIBUTING.md) section.
+
+# License
+
+[MIT](LICENSE)
---
+++ src/client/components/Video.js
@@ -0,0 +1,62 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import classnames from 'classnames'
+import { ME } from '../constants.js'
+import { MediaStream } from '../window.js'
+
+export const StreamPropType = PropTypes.shape({
+  mediaStream: PropTypes.instanceOf(MediaStream).isRequired,
+  url: PropTypes.string
+})
+
+export default class Video extends React.PureComponent {
+  static propTypes = {
+    onClick: PropTypes.func,
+    active: PropTypes.bool.isRequired,
+    stream: StreamPropType,
+    userId: PropTypes.string.isRequired
+  }
+  handleClick = e => {
+    const { onClick, userId } = this.props
+    this.play(e)
+    onClick(userId)
+  }
+  play = e => {
+    e.preventDefault()
+    e.target.play()
+  }
+  componentDidMount () {
+    this.componentDidUpdate()
+  }
+  componentDidUpdate () {
+    const { stream } = this.props
+    const { video } = this.refs
+    const mediaStream = stream && stream.mediaStream
+    const url = stream && stream.url
+    if ('srcObject' in video) {
+      if (video.srcObject !== mediaStream) {
+        this.refs.video.srcObject = mediaStream
+      }
+    } else {
+      if (video.src !== url) {
+        video.src = url
+      }
+    }
+  }
+  render () {
+    const { active, userId } = this.props
+    const className = classnames('video-container', { active })
+    return (
+      <div className={className}>
+        <video
+          autoPlay
+          muted={userId === ME}
+          onClick={this.handleClick}
+          onLoadedMetadata={this.play}
+          playsInline
+          ref="video"
+        />
+      </div>
+    )
+  }
+}
